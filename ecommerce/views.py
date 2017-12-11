346841693df5d73d69255bfa2e1a278b782d8eb9
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-11 21:41:48 +0530

    Replace forms with default django forms

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 
 def home_page(request):
 	context = {
@@ -19,9 +21,11 @@ def about_page(request):
 
 
 def contact_page(request):
+	contact_form = ContactForm()
 	context = {
 		"title": "Contact",
-		"content": "This is the contact page."
+		"content": "This is the contact page.",
+		"form": contact_form
 	}
 	if request.method == 'POST':
 		# print(request.POST)
