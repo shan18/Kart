d88cb43e73f8d36705893c1f763531bac3e9f666
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-11 20:39:15 +0530

    Adding contact and about page.

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,4 +3,28 @@ from django.shortcuts import render
 
 
 def home_page(request):
+	context = {
+		"title": "Home",
+		"content": "Welcome to the homepage."
+	}
+	return render(request, "home_page.html", context)
+
+
+def about_page(request):
+	context = {
+		"title": "About",
+		"content": "This is the about page."
+	}
+	return render(request, "home_page.html", context)
+
+
+def contact_page(request):
+	context = {
+		"title": "Contact",
+		"content": "This is the contact page."
+	}
+	return render(request, "home_page.html", context)
+
+
+def home_page_old(request):
 	return HttpResponse("<h1>Hello World!</h1>")
