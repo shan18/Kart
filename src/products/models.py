81bf56d1c7db28ea29027e641e6ab213e696c176
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-13 22:33:17 +0530

    Add Product model and register it to admin portal

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
