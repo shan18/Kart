f776271366e49295066f6b2527b303e40dbb9276
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-15 19:10:24 +0530

    Add detail view for each product

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -14,6 +14,8 @@ urlpatterns = [
     url(r'^register/$', register_page),
     url(r'^products/$', ProductListView.as_view()),
     url(r'^products-fbv/$', product_list_view),
+    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
     url(r'^admin/', admin.site.urls),
 ]
 
--- src/products/views.py
+++ src/products/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Product
+
+
+""" Class Based Views
+"""
 
 
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'products/list.html'
 
+    # use this to check which key in context has the data, then use that in the template
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
     #     print(context)
     #     return(context)
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+
+""" Function Based Views
+"""
 
 
 def product_list_view(request):
@@ -20,3 +34,12 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, 'products/list.html', context)
+
+
+def product_detail_view(request, pk, *args, **kwargs):
+    # instance = Product.objects.get(pk=pk)
+    instance = get_object_or_404(Product, pk=pk)    # If pk not in arg list, then pk=kwargs['pk']
+    context = {
+        'object': instance
+    }
+    return render(request, 'products/detail.html', context)
