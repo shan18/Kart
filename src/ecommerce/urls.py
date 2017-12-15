5e8a9b10d419573f0873cb37b7f03936b3bd9fec
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-15 17:37:47 +0530

    Add a List view of products

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from products.views import ProductListView, product_list_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -11,6 +12,8 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
+    url(r'^products/$', ProductListView.as_view()),
+    url(r'^products-fbv/$', product_list_view),
     url(r'^admin/', admin.site.urls),
 ]
 
--- src/products/models.py
+++ src/products/models.py
@@ -5,3 +5,9 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
+
+    def __str__(self):  # For python 3
+        return self.title
+
+    def __unicode__(self):  # For python 2
+        return self.title
