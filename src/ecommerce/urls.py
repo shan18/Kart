14ad0c021f151b7fe565d1a11f154b1363959de7
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-18 16:10:18 +0530

    Add image for each product

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/products/models.py
+++ src/products/models.py
@@ -1,10 +1,31 @@
+import os
+import random
+
 from django.db import models
+
+
+def get_filename_extension(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    # print(instance)
+    # print(filename)
+    new_filename = random.randint(1, 5890349)
+    name, ext = get_filename_extension(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return 'products/{new_filename}/{final_filename}'.format(
+        new_filename=new_filename, final_filename=final_filename
+    )
 
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):  # For python 3
         return self.title
