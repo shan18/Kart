a282ffe978f4947b0e340831f4f82239c4f7e76a
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-19 20:57:42 +0530

    create separate urls file for products app and remove all unnecessary urls

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -1,17 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from products.views import (
-        ProductListView,
-        product_list_view,
-        ProductDetailView,
-        ProductDetailSlugView,
-        product_detail_view,
-        ProductFeaturedListView,
-        ProductFeaturedDetailView
-    )
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -20,13 +11,7 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
-    url(r'^products/$', ProductListView.as_view()),
-    url(r'^products-fbv/$', product_list_view),
-    # url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
-    url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
-    url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
-    url(r'^featured/$', ProductFeaturedListView.as_view()),
-    url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
+    url(r'^products/', include('products.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
---
+++ src/products/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from products.views import (
+        ProductListView,
+        ProductDetailSlugView
+    )
+
+urlpatterns = [
+    url(r'^$', ProductListView.as_view()),
+    url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
+]
