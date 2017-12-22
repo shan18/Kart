e6d0094a9b7206184963d66d3f38e21f517e4094
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-22 19:04:57 +0530

    Enable tag based search

--- src/products/models.py
+++ src/products/models.py
@@ -36,7 +36,12 @@ class ProductQuerySet(models.query.QuerySet):
         return self.filter(featured=True)
 
     def search(self, query):
-        lookups = Q(title__icontains=query) | Q(description__icontains=query) | Q(price__icontains=query)
+        lookups = (
+            Q(title__icontains=query) |
+            Q(description__icontains=query) |
+            Q(price__icontains=query) |
+            Q(tag__title__icontains=query)
+        )
         return self.filter(lookups).distinct()
 
 
