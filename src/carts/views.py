df6818252bdda43d834aa970342563564832dd06
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-24 02:52:49 +0530

    Add session id based cart view

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+from .models import Cart
+
+
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    print('New Cart created')
+    return cart_obj
+
 
 def cart_home(request):
     cart_id = request.session.get('cart_id', None)
-    if cart_id is None:  # and isinstance(cart_id, int):
-        print('create new cart')
-        request.session['cart_id'] = 18
-    else:
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
         print('Cart ID exists')
+        cart_obj = qs.first()
+    else:  # This block will be executed when no such id exists or id cotntains non-numeric characters
+        cart_obj = cart_create()
+        request.session['cart_id'] = cart_obj.id
     return render(request, 'carts/home.html', {})
