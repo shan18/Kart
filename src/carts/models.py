74ca17064fbb5ddc432b1a6f6d7eee755e823ab3
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-24 03:07:12 +0530

    Add user model to cart sessions

--- src/carts/models.py
+++ src/carts/models.py
@@ -7,12 +7,22 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 
+class CartManager(models.Manager):
+    def new(self, user=None):
+        user_obj = None
+        if user is not None and user.is_authenticated():
+            user_obj = user
+        return self.model.objects.create(user=user_obj)
+
+
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True)  # Non-logged in user can also create a cart
     products = models.ManyToManyField(Product, blank=True)  # Cart can be blank
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)  # Last updated time
     timestamp = models.DateTimeField(auto_now_add=True)  # Created time
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
--- src/carts/views.py
+++ src/carts/views.py
@@ -3,19 +3,16 @@ from django.shortcuts import render
 from .models import Cart
 
 
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print('New Cart created')
-    return cart_obj
-
-
 def cart_home(request):
     cart_id = request.session.get('cart_id', None)
     qs = Cart.objects.filter(id=cart_id)
     if qs.count() == 1:
         print('Cart ID exists')
         cart_obj = qs.first()
+        if request.user.is_authenticated() and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:  # This block will be executed when no such id exists or id cotntains non-numeric characters
-        cart_obj = cart_create()
+        cart_obj = Cart.objects.new(user=request.user)
         request.session['cart_id'] = cart_obj.id
     return render(request, 'carts/home.html', {})
