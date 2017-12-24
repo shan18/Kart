8bcd75d26a3d2954bc5f3d5a6c2b2f71d796b075
Shantanu Acharya <thegeek.004@gmail.com>
2017-12-24 17:09:53 +0530

    add cart form

--- src/carts/views.py
+++ src/carts/views.py
@@ -10,11 +10,17 @@ def cart_home(request):
 
 
 def cart_update(request):
-    product_id = 1
-    product_obj = Product.objects.get(id=product_id)
-    cart_obj, new_obj = Cart.objects.get_or_new(request)
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj)
-    else:
-        cart_obj.products.add(product_obj)
+    # print(request.POST)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print("Show message to user, product gone?")
+            return redirect('cart:home')
+        cart_obj, new_obj = Cart.objects.get_or_new(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
     return redirect('cart:home')
