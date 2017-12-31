a39229487c6f5b00294fc7b5dc193211af1f1588
Shantanu Acharya <thegeek.004@gmail.com>
2018-01-01 02:19:17 +0530

    Add Creation and Updation form for custom user in Admin Panel

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,25 +1,54 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 # OR --> from .models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 from .models import GuestModel
 
 
 User = get_user_model()
 
-class UserAdmin(admin.ModelAdmin):
-    search_fields = ['email']   # Search users by email
 
-    class Meta:
-        model = User
+class UserAdmin(BaseUserAdmin):
+    # The forms to change and add user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'admin')
+    list_filter = ('admin', 'staff', 'active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ()}),  # would have data we had fields like 'full_name'
+        ('Permissions', {'fields': ('admin', 'staff', 'active')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
 
 admin.site.register(User, UserAdmin)
 
+admin.site.unregister(Group)  # Remove Group Model from admin. We're not using it.
+
 
 class GuestModelAdmin(admin.ModelAdmin):
-    search_fields = ['email']   # Search users by email
+    search_fields = ['email']   # Search guest users by email in admin panel
 
     class Meta:
         model = GuestModel
+
 
 admin.site.register(GuestModel, GuestModelAdmin)
