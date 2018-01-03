e2fe2775553372972e761b1168efde4d3c97a60c
Shantanu Acharya <thegeek.004@gmail.com>
2018-01-03 19:57:24 +0530

    add stripe quickstart element

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+import stripe
+
+from billing import credentials
+
+
+stripe.api_key = credentials.SECRET_KEY
+STRIPE_PUB_KEY = credentials.PUBLISH_KEY
+
+
+def payment_method_view(request):
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'billing/payment_method.html', {"publish_key": STRIPE_PUB_KEY})
--- src/kart/urls.py
+++ src/kart/urls.py
@@ -9,6 +9,7 @@ from .views import home_page, about_page, contact_page
 from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from carts.views import cart_home_api_view
+from billing.views import payment_method_view
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
@@ -22,6 +23,7 @@ urlpatterns = [
     url(r'^checkout/address/reuse/$', checkout_address_reuse_view, name='checkout_address_reuse'),
     url(r'^api/cart/$', cart_home_api_view, name='api-cart'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
+    url(r'^billing/payment-method/$', payment_method_view, name='billing-payment-method'),
     url(r'^products/', include('products.urls', namespace='products')),
     url(r'^search/', include('search.urls', namespace='search')),
     url(r'^cart/', include('carts.urls', namespace='cart')),
