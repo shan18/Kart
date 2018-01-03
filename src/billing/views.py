fd710c6076a1bac075b2d3cc507735f334d8c1c0
Shantanu Acharya <thegeek.004@gmail.com>
2018-01-04 01:10:34 +0530

    improvements in stripe payment form and add a payment add create view

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.utils.http import is_safe_url
 
 import stripe
 
@@ -10,6 +12,16 @@ STRIPE_PUB_KEY = credentials.PUBLISH_KEY
 
 
 def payment_method_view(request):
-    if request.method == 'POST':
+    next_url = None
+    next_ = request.GET.get('next')
+    if is_safe_url(next_, request.get_host()):
+        next_url = next_
+
+    return render(request, 'billing/payment_method.html', {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
+
+
+def payment_method_createview(request):
+    if request.method == 'POST' and request.is_ajax():
         print(request.POST)
-    return render(request, 'billing/payment_method.html', {"publish_key": STRIPE_PUB_KEY})
+        return JsonResponse({'message': 'Done!'})
+    return HttpResponse('error', status_code=401)
--- src/kart/urls.py
+++ src/kart/urls.py
@@ -9,7 +9,7 @@ from .views import home_page, about_page, contact_page
 from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from carts.views import cart_home_api_view
-from billing.views import payment_method_view
+from billing.views import payment_method_view, payment_method_createview
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^api/cart/$', cart_home_api_view, name='api-cart'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^billing/payment-method/$', payment_method_view, name='billing-payment-method'),
+    url(r'^billing/payment-method/create/$', payment_method_createview, name='billing-payment-method-endpoint'),
     url(r'^products/', include('products.urls', namespace='products')),
     url(r'^search/', include('search.urls', namespace='search')),
     url(r'^cart/', include('carts.urls', namespace='cart')),
