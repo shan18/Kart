90663ec739920dc18b0d30319fb736d39dbabeab
Shantanu Acharya <thegeek.004@gmail.com>
2018-01-07 22:02:45 +0530

    mailchimp: add guest model to subscription list

--- src/marketing/admin.py
+++ src/marketing/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import MarketingPreference
+from .models import MarketingPreference, GuestModelMarketingPreference
 
 
 class MarketingPreferenceAdmin(admin.ModelAdmin):
@@ -11,8 +11,23 @@ class MarketingPreferenceAdmin(admin.ModelAdmin):
         model = MarketingPreference
         fields = [
             'user', 'subscribed',
-            'mailchimp_subscribed', 'mailchimp_msg', 'timestamp', 'update'
+            'mailchimp_subscribed', 'mailchimp_msg', 'timestamp', 'updated'
         ]
 
 
 admin.site.register(MarketingPreference, MarketingPreferenceAdmin)
+
+
+class GuestModelMarketingPreferenceAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'subscribed', 'updated']
+    readonly_fields = ['mailchimp_msg', 'mailchimp_subscribed', 'timestamp', 'updated']
+
+    class Meta:
+        model = MarketingPreference
+        fields = [
+            'user', 'subscribed',
+            'mailchimp_subscribed', 'mailchimp_msg', 'timestamp', 'updated'
+        ]
+
+
+admin.site.register(GuestModelMarketingPreference, GuestModelMarketingPreferenceAdmin)
--- src/marketing/models.py
+++ src/marketing/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models.signals import post_save, pre_save
 
 from .utils import Mailchimp
+from accounts.models import GuestModel
 
 
 User = settings.AUTH_USER_MODEL
@@ -56,3 +57,53 @@ def marketing_preference_update_receiver(sender, instance, *args, **kwargs):
         instance.mailchimp_msg = response_data
 
 pre_save.connect(marketing_preference_update_receiver, sender=MarketingPreference)
+
+
+class GuestModelMarketingPreference(models.Model):
+    guest_user = models.OneToOneField(GuestModel)
+    subscribed = models.BooleanField(default=True)
+    mailchimp_subscribed = models.NullBooleanField(blank=True)
+    mailchimp_msg = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.guest_user.email
+
+
+def guest_marketing_pref_receiver(sender, instance, created, *args, **kwargs):
+    '''
+    Create marketing preference when guest model is created
+    '''
+    if created:
+        GuestModelMarketingPreference.objects.get_or_create(guest_user=instance)
+
+post_save.connect(guest_marketing_pref_receiver, sender=GuestModel)
+
+
+def guest_marketing_preference_create_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        status_code, response_data = Mailchimp().subscribe(instance.guest_user.email)
+
+post_save.connect(guest_marketing_preference_create_receiver, sender=GuestModelMarketingPreference)
+
+
+def guest_marketing_preference_update_receiver(sender, instance, *args, **kwargs):
+    ''' When user changes the subscription status, it updates the mailchimp db
+    '''
+    if instance.subscribed != instance.mailchimp_subscribed:
+        if instance.subscribed:
+            status_code, response_data = Mailchimp().subscribe(instance.guest_user.email)
+        else:
+            status_code, response_data = Mailchimp().unsubscribe(instance.guest_user.email)
+
+        if response_data['status'] == 'subscribed':
+            instance.subscribed = True
+            instance.mailchimp_subscribed = True
+        else:
+            instance.subscribed = False
+            instance.mailchimp_subscribed = False
+
+        instance.mailchimp_msg = response_data
+
+pre_save.connect(guest_marketing_preference_update_receiver, sender=GuestModelMarketingPreference)
