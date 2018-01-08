c48f7076ce3342a8da3247e5d1dcf2cc3fc5287b
Shantanu Acharya <thegeek.004@gmail.com>
2018-01-09 04:00:10 +0530

    Add AWS Configuration files

---
+++ src/kart/aws/utils.py
@@ -0,0 +1,6 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+# location of static and media inside bucket
+StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
+MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
