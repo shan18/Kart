0a7203da1b931bb66827d2285227bc9ba4740324
Shantanu Acharya <thegeek.004@gmail.com>
2018-01-08 18:54:13 +0530

    new settings module

--- src/kart/settings/base.py
+++ src/kart/settings/base.py
@@ -16,7 +16,7 @@ from kart import credentials
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
