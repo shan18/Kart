d0f3db81e4b024931343bc87282c7f2ac69b2a71
Shantanu Acharya <thegeek.004@gmail.com>
2018-01-23 11:47:53 +0530

    fix address cascade delete issue

--- src/orders/models.py
+++ src/orders/models.py
@@ -116,8 +116,8 @@ class OrderManager(models.Manager):
 class Order(models.Model):
     order_id = models.CharField(max_length=120, blank=True)
     billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True)
-    shipping_address = models.ForeignKey(Address, related_name='shipping_address', null=True, blank=True)
-    billing_address = models.ForeignKey(Address, related_name='billing_address', null=True, blank=True)
+    shipping_address = models.ForeignKey(Address, related_name='shipping_address', null=True, blank=True, on_delete=models.SET_NULL)
+    billing_address = models.ForeignKey(Address, related_name='billing_address', null=True, blank=True, on_delete=models.SET_NULL)
     cart = models.ForeignKey(Cart)
     status = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
